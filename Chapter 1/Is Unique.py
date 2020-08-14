bef4913d580ef1ae913fe4750960255ae0d006d5
Brian <brianruiz123@yahoo.com>
2020-08-13 17:43:33 -0700

    is unique brute force solution

--- Chapter 1/Is Unique.py
+++ Chapter 1/Is Unique.py
@@ -2,3 +2,17 @@
 # Implement an algorithm to determine if a string has all unique characters. 
 # What if you cannot use additional data structures?
 
+class Solution():
+    def isUnique(self, s):
+        unique = None
+        string_map = {}
+
+        for letter in s:
+            if letter in string_map:
+                unique = False
+                break
+            else:
+                string_map[letter] = 0
+                unique = True
+        
+        return unique
